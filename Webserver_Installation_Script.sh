e21cb9a137f06ea413e78819d9ea2ead4552883b
AcidzDesigns <acidzdesigns@gmail.com>
2018-06-16 23:52:36 +0100

    Update Webserver_Installation_Script.sh

--- Webserver_Installation_Script.sh
+++ Webserver_Installation_Script.sh
@@ -75,7 +75,7 @@ echo "
 "
 sleep 1
 
-add-apt-repository -y ppa:ondrej/php
+add-apt-repository -y ppa:ondrej/php -y
 apt update
 apt -y install php7.2 php7.2-cli php7.2-gd php7.2-mysql php7.2-pdo php7.2-mbstring php7.2-tokenizer php7.2-bcmath php7.2-xml php7.2-fpm php7.2-curl php7.2-zip
 
